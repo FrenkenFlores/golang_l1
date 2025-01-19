ea1701f7ebf6a06735d464e9a21d65c153eb684b
FrenkenFlores <saifualdin.evloev@gmail.com>
2025-01-19 15:50:41 +0300

    update #2: L1.2 - sync, chan, goroutine
    Signed-off-by: FrenkenFlores <saifualdin.evloev@gmail.com>

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/FrenkenFlores/golang_l1/l1_1"
+	"github.com/FrenkenFlores/golang_l1/l1_2"
 )
 
 func main() {
@@ -11,6 +12,11 @@ func main() {
 		fmt.Println("TEST L1.1")
 		a := l1_1.Action{Human: l1_1.Human{Name: "Adam"}}
 		fmt.Println(a.GetName())
+	}
+	{
+		fmt.Println("TEST L1.2")
+		a := l1_2.GetSquareList([]int{2, 4, 6, 8, 10})
+		fmt.Println(a)
 	}
 
 }
---
+++ l1_2/l1_2.go
@@ -0,0 +1,24 @@
+package l1_2
+
+import "sync"
+
+func getSquare(a int, wg *sync.WaitGroup, ch chan<- int) {
+	defer wg.Done()
+	ch <- a * a
+}
+
+func GetSquareList(a []int) []int {
+	var wg sync.WaitGroup
+	ch := make(chan int, len(a))
+	for _, i := range a {
+		wg.Add(1)
+		go getSquare(i, &wg, ch)
+	}
+	wg.Wait()
+	var res []int = make([]int, len(a))
+	for i := 0; i < len(a); i++ {
+		res[i] = <-ch
+	}
+
+	return res
+}
